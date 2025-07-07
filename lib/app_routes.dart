ca3028ada4daf895d3270846ae3465140964e1c0
Lucas Maciel <2008lucasa@gmail.com>
2025-07-07 17:04:02 -0300

    Criação das rotas para cada tela

---
+++ lib/app_routes.dart
@@ -0,0 +1,12 @@
+class AppRoutes {
+  static const home = '/';
+  static const login = '/login';
+  static const cadastro = '/cadastro';
+  static const perfil = '/perfil';
+  static const produtos = '/produtos';
+  static const homepage = '/homepage';
+  static const estoque = '/estoque';
+  static const registroBaixas = '/registro_baixas';
+  static const cadastroProduto = '/cadastro_produto';
+  static const cadastroEstoque = '/cadastro_estoque';
+}
